6c111d0a562b239fff637f99b31fbc8672d00c0c
Dev Aggarwal <devxpy@gmail.com>
2019-04-26 02:35:30 +0530

    migrations + remove mediafiles

--- firmware_release_server/main.py
+++ firmware_release_server/main.py
@@ -7,6 +7,8 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firmware_release_server.settings")
 django.setup()
 
+from django.core.management import call_command
+
 try:
     production = sys.argv[1] == "production"
 except IndexError:
@@ -15,11 +17,12 @@ except IndexError:
 if production:
     import gunicorn.app.wsgiapp as wsgi
 
+    call_command("migrate")
+    call_command("collectstatic", "--noinput")
+
     # This is just a simple way to supply args to gunicorn
     sys.argv = [".", "firmware_release_server.wsgi", "--bind=0.0.0.0:80"]
 
     wsgi.run()
 else:
-    from django.core.management import call_command
-
     call_command("runserver")
