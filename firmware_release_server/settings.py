db32e8a450aa1c15887fd20959031bf6af9163f1
Dev Aggarwal <devxpy@gmail.com>
2019-05-31 02:41:56 +0530

    typo

--- firmware_release_server/settings.py
+++ firmware_release_server/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "firmware_uploads.apps.FirmwareUploadsConfig",
-    "products.apps.noinput",
+    "products.apps.ProductsConfig",
     "rest_api",
 ]
 
