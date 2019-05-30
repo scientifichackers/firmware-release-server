f61a8e596eb5a84d358dceae7b86f958f09957b4
Dev Aggarwal <devxpy@gmail.com>
2019-05-31 02:44:23 +0530

    captain

---
+++ build-zip.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# clean old build
+rm -r dist app.zip
+
+# include the dependencies from `pip freeze`
+pip install -r requirements/main.txt --target dist
+
+# specify which files to be included in the build
+# You probably want to specify what goes here
+cp -r firmware_release_server firmware_uploads products rest_api manage.py dist
+
+cd dist
+
+zip -qr ../app.zip *
--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
-wget -O app.zip https://github.com/pycampers/firmware-release-server/releases/download/(cat firmware_release_server/__version__ )/app.zip
+wget -O app.zip https://github.com/pycampers/firmware-release-server/releases/download/$(cat firmware_release_server/__version__ )/app.zip
 unzip app.zip
 nginx
 python manage.py migrate
