aa348e569bd10d444320758c88a82db130dd57d9
Dev Aggarwal <devxpy@gmail.com>
2019-05-31 04:07:59 +0530

    scripts

--- runserver.sh
+++ runserver.sh
@@ -1,14 +1,17 @@
 set -x
 
+# download zip
 wget -qO app.zip "https://github.com/pycampers/firmware-release-server/releases/download/$(cat __version__ )/app.zip"
 unzip -q app.zip -d app
 
+# set paths
 cd app
 export PYTHONPATH=$PYTHONPATH:$PWD
 
+# pre-run stuff
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+# run the server
 nginx
-
 python bin/gunicorn --bind=unix:/home/firmware_release_server.sock firmware_release_server.wsgi
