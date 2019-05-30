e60ffd9315d93238d934dd81d13e72d6203c32f4
Dev Aggarwal <devxpy@gmail.com>
2019-05-31 02:47:22 +0530

    fix version stuff

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,6 @@
-wget -O app.zip https://github.com/pycampers/firmware-release-server/releases/download/$(cat firmware_release_server/__version__ )/app.zip
-unzip app.zip
+wget -O app.zip https://github.com/pycampers/firmware-release-server/releases/download/$(cat __version__ )/app.zip
+unzip app.zip -d app
+cd app
 nginx
 python manage.py migrate
 python manage.py collectstatic --noinput
