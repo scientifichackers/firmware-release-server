d7676d5c456fad7b86eddafe75556b932458db09
Dev Aggarwal <devxpy@gmail.com>
2019-05-31 02:38:13 +0530

    CAPTAIN

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,5 @@
+wget -O app.zip https://github.com/pycampers/firmware-release-server/releases/latest/download/app.zip
+unzip -q app.zip
 nginx
 python manage.py migrate
 python manage.py collectstatic --noinput
