4172ba4f194b77f3ba1561cac6b25604e5195b0e
Dev Aggarwal <devxpy@gmail.com>
2019-05-31 03:21:19 +0530

    migrations studd

--- runserver.sh
+++ runserver.sh
@@ -1,20 +1,16 @@
-echo "downloading app.zip ..."
+set -x
+
 wget -qO app.zip "https://github.com/pycampers/firmware-release-server/releases/download/$(cat __version__ )/app.zip"
 
-echo "unzipping ..."
 unzip -q app.zip -d app
 cd app
 
-echo "nginx"
 nginx
 
-echo "$ manage.py migrate"
 python manage.py migrate
 
-echo "$ python manage.py collectstatic --noinput"
 python manage.py collectstatic --noinput
 
 export PYTHONPATH=$PYTHONPATH:$PWD
 
-echo "$ python bin/gunicorn --bind=unix:/home/firmware_release_server.sock firmware_release_server.wsgi"
 python bin/gunicorn --bind=unix:/home/firmware_release_server.sock firmware_release_server.wsgi
