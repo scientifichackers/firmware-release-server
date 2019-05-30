38d81472aa1d6e4811d770f4bdeb8ba8d9fa9e92
Dev Aggarwal <devxpy@gmail.com>
2019-05-31 02:50:34 +0530

    captain

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,7 @@
-wget -O app.zip https://github.com/pycampers/firmware-release-server/releases/download/$(cat __version__ )/app.zip
-unzip app.zip -d app
+wget -qO app.zip https://github.com/pycampers/firmware-release-server/releases/download/$(cat __version__ )/app.zip
+unzip -q app.zip -d app
 cd app
 nginx
 python manage.py migrate
 python manage.py collectstatic --noinput
-./bin/gunicorn --bind=unix:/home/firmware_release_server.sock firmware_release_server.wsgi
+./app/bin/gunicorn --bind=unix:/home/firmware_release_server.sock firmware_release_server.wsgi
